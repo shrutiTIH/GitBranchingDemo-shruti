79dfb6fa45c4f0018faa5da6043389eded2ab09e
Shruti Bagadia <shruti.bagadia@tihiitb.org>
2025-12-18 14:53:08 +0530

    divide

--- calculator.py
+++ calculator.py
@@ -4,6 +4,9 @@
 def subtract(x, y):
     return x - y
 
+def multiply(x, y):
+    return x * y
+
 def divide(x, y):
     if y == 0:
         return "Error! Division by zero."
@@ -13,9 +16,10 @@ def main():
     print("Simple Calculator")
     print("1. Add")
     print("2. Subtract")
-    print("3. Divide")
+    print("3. Multiply")
+    print("4. Divide")
     
-    choice = input("Enter choice(1/2/3): ")
+    choice = input("Enter choice(1/2/3/4): ")
     
     if choice == '1':
         num1 = float(input("Enter first number: "))
@@ -26,6 +30,10 @@ def main():
         num2 = float(input("Enter second number: "))
         print(f"{num1} - {num2} = {subtract(num1, num2)}")
     elif choice == '3':
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+        print(f"{num1} * {num2} = {multiply(num1, num2)}")
+    elif choice == '4':
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
         print(f"{num1} / {num2} = {divide(num1, num2)}")
